7686ed3dbbd89611bda679e6b991d05a87af19af
Brian Hestir <brhestir@gmail.com>
2020-12-17 23:55:15 -0500

    Updated render code, moved asset folders

---
+++ assets/js/script.js
@@ -0,0 +1,59 @@
+// script.js
+$(document).ready(function() {
+  var calendarArray = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+
+  $("#currentDay").text(moment().format("MMMM Do YYYY"));
+
+  function main() {
+    if (localStorage.getItem("calendarArray") !== null) {
+      calendarArray = JSON.parse(localStorage.getItem("calendarArray"));
+    } else {
+      localStorage.setItem("calendarArray", JSON.stringify(calendarArray));
+    }
+    drawCalendar();
+  }
+
+  main();
+
+  function drawCalendar() {
+    for (var i = 9; i < 18; i++) {
+      var timeText;
+      if (i === 9) {
+        timeText = moment("2013-02-08 0" + i).format("LT");
+      } else {
+        timeText = moment("2013-02-08 " + i).format("LT");
+      }
+      var timeBlock = $("<div>");
+      timeBlock.addClass("row time-block");
+      var hourEl = $("<div>");
+      hourEl.addClass("col-1 hour");
+      hourEl.text(timeText);
+      var textareaEl = $("<textarea>");
+      textareaEl.addClass("col-10");
+      textareaEl.val(calendarArray[i - 9]);
+      if (moment().hour() > i) {
+        textareaEl.addClass("past");
+      } else if (moment().hour() == i) {
+        textareaEl.addClass("present");
+      } else {
+        textareaEl.addClass("future");
+      }
+      var saveButton = $("<button>");
+      saveButton.addClass("col-1  fas fa-save saveBtn");
+      saveButton.attr("data-time", i);
+      $("#main").append(timeBlock);
+      timeBlock.append(hourEl);
+      timeBlock.append(textareaEl);
+      timeBlock.append(saveButton);
+    }
+  }
+
+  function storeCalendar(event) {
+    calendarArray[parseInt($(this).attr("data-time")) - 9] = $(this)
+      .prev()
+      .val();
+    localStorage.setItem("calendarArray", JSON.stringify(calendarArray));
+  }
+
+  $("#main").on("click", ".saveBtn", storeCalendar);
+});
