94837a8b20ef5696defed1251e13587bb52ad252
Brian Hestir <brhestir@gmail.com>
2020-12-17 23:36:17 -0500

    Added main

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 // script.js
+$(document).ready(function() {
+  var calendarArray = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+
+  $("#currentDay").text(moment().format("MMMM Do YYYY"));
+
+  function main() {
+    if (localStorage.getItem("calendarArray") !== null) {
+      calendarArray = JSON.parse(localStorage.getItem("calendarArray"));
+    } else {
+      localStorage.setItem("calendarArray", JSON.stringify(calendarArray));
+    }
+    renderCalendar();
+  }
+
+  main();
